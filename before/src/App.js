e02f2e3a26609582638e4fa8df33e3e7c04a6aef
liufan <1014229769@qq.com>
2022-12-11 13:43:02 +0800

    引入antdesign

--- before/src/App.js
+++ before/src/App.js
@@ -3,17 +3,50 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login';
 import Layout from './pages/Layout';
 
-function App() {
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import { ConfigProvider, DatePicker, message } from 'antd';
+// 由于 antd 组件的默认文案是英文，所以需要修改为中文
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
+import zhCN from 'antd/locale/zh_CN';
+import 'antd/dist/reset.css';
+import './index.scss';
+
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <div className="App">
+//         <Routes>
+//           <Route path='/' element={<Layout></Layout>}></Route>
+//           <Route path='/login' element={<Login></Login>}></Route>
+//         </Routes>
+//       </div>
+//     </BrowserRouter>
+//   );
+// }
+
+dayjs.locale('zh-cn');
+
+const App = () => {
+  const [date, setDate] = useState(null);
+  const handleChange = value => {
+    message.info(`您选择的日期是: ${value ? value.format('YYYY年MM月DD日') : '未选择'}`);
+    setDate(value);
+  };
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path='/' element={<Layout></Layout>}></Route>
-          <Route path='/login' element={<Login></Login>}></Route>
-        </Routes>
+    <ConfigProvider locale={zhCN}>
+      <div style={{ width: 400, margin: '100px auto' }}>
+        <DatePicker onChange={handleChange} />
+        <div style={{ marginTop: 16 }}>
+          当前日期：{date ? date.format('YYYY年MM月DD日') : '未选择'}
+        </div>
       </div>
-    </BrowserRouter>
+    </ConfigProvider>
   );
-}
+};
+
+render(<App />, document.getElementById('root'))
 
 export default App;
