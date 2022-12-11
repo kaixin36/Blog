e3f5065784d20654e84b8a41b8a5de49acdebf4e
liufan <1014229769@qq.com>
2022-12-11 13:10:34 +0800

    添加路由

--- before/src/App.js
+++ before/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login';
+import Layout from './pages/Layout';
 
 function App() {
   return (
-    <div className="App">
-   
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Layout></Layout>}></Route>
+          <Route path='/login' element={<Login></Login>}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
