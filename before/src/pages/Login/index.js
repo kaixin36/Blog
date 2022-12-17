420b55de013aa6118b04b3bdb1718b52417b2291
liufan <1014229769@qq.com>
2022-12-17 16:19:36 +0800

    登录界面ui

--- before/src/pages/Login/index.js
+++ before/src/pages/Login/index.js
@@ -1,13 +1,77 @@
-import { Card } from 'antd'
+import { Card, Form, Input, Button, Checkbox } from 'antd'
 import './index.scss'
 
 function Login() {
     return (
         <>
-            <Card title="登录" className='login'>
-                <p>用户名</p>
-                <p>密码</p>
-            </Card>
+            <div className='login_div'>
+                <Card title="极客园" className='login'>
+                    <Form
+                        name="basic"
+                        labelCol={{
+                            span: 8,
+                        }}
+                        wrapperCol={{
+                            span: 16,
+                        }}
+                        initialValues={{
+                            remember: true,
+
+                        }}
+                        // onFinish={onFinish}
+                        //onFinishFailed={onFinishFailed}
+                        autoComplete="off"
+                    >
+                        <Form.Item
+                            label="用户名"
+                            name="username"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: '请输入用户名!',
+                                },
+                            ]}
+                        >
+                            <Input />
+                        </Form.Item>
+
+                        <Form.Item
+                            label="密码"
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: '请输入密码!',
+                                },
+                            ]}
+                        >
+                            <Input.Password />
+                        </Form.Item>
+
+                        <Form.Item
+                            name="remember"
+                            valuePropName="checked"
+                            wrapperCol={{
+                                offset: 8,
+                                span: 16,
+                            }}
+                        >
+                            <Checkbox>记住当前登录信息</Checkbox>
+                        </Form.Item>
+
+                        <Form.Item
+                            wrapperCol={{
+                                offset: 8,
+                                span: 16,
+                            }}
+                        >
+                            <Button type="primary" htmlType="submit">
+                                Submit
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </Card>
+            </div>
         </>
     )
 }
